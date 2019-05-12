f441840b12665166512e51c20467ef9382a98109
Jennifer Louthan <jenny@uncorkedstudios.com>
2019-05-11 21:32:26 -0400

    Refactored everything into Tween animcations

--- js/particle-globe.js
+++ js/particle-globe.js
@@ -4,179 +4,90 @@
 // https://www.delimited.io/blog/2015/5/16/interactive-webgl-globes-with-threejs-and-d3
 // http://bl.ocks.org/MaciejKus/61e9ff1591355b00c1c1caf31e76a668
 
-// Draw the map for the default Earth texture so it's ready when we create mesh
-let worldMap = new WorldMap();
-worldMap.createDrawing().then(function () {
-
-// Create a scene with a camera and renderer
-let scene = new THREE.Scene();
-
 const FIELD_OF_VIEW = 100;
 const NEAR = 0.01;
 const FAR = 1000;
 const EARTH_RAD = 0.99;
 const CLOUD_RAD = EARTH_RAD + 0.01;
 const NUM_PARTICLES = 1000;
-let camera	= new THREE.PerspectiveCamera(
-  FIELD_OF_VIEW,
-  window.innerWidth / window.innerHeight,
-  NEAR,
-  FAR
-);
-camera.position.z = 1.5;
 
-
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// Add some light
-// TODO: play with this so it's not exactly same as demo
-let light	= new THREE.AmbientLight(0x888888);
-scene.add(light);
-let dirLight	= new THREE.DirectionalLight(0xcccccc, 0.5);
-dirLight.position.set(5, 3, 5);
-scene.add(dirLight);
-
-// Create and add the Earth
-let earth = new Earth(EARTH_RAD, false, worldMap.canvas);
-if (earth.waterMesh) {
-  scene.add(earth.waterMesh);
-}
-scene.add(earth.mesh);
-earth.addClouds();
-
-let particleCloud = new ParticleCloud(
-  NUM_PARTICLES,
-  0.01,
-  0xffffff,
-  earth.cloudMesh,
-  earth.cloudRadius
-);
-earth.mesh.add(particleCloud.mesh);
-
-// Render and update loop
-let clock = new THREE.Clock();
-
-// TODO make these nicer
 let isRotating = true;
-let isZooming = false;
-let isZoomingOut = false;
 let isZoomedIn = false;
-let expandingParticles = false;
-let expansionOffset = 0;
 const EXPANSION_MAX = 0.2;
-let shrinkingParticles = false;
-let shrinkingOffset = EXPANSION_MAX;
-const SHRINKING_MIN = 0.01
-// TODO in the end, after all animation, particles expand out infinitely and
-// become stars!
+const AMOUNT_POINT_ABOVE = 0.02;
 
-let initialFacingCamera = new THREE.Vector3(0, 0, 1);
+let earth;
+let particleCloud;
+let camera;
 
-function render(){
-  requestAnimationFrame(render);
+// Draw the map for the default Earth texture so it's ready when we create mesh
+let worldMap = new WorldMap();
+worldMap.createDrawing().then(function () {
 
-  let timeDelta = clock.getDelta(); // in seconds
-  if (isRotating) {
-    // Rotate Water
-    // waterMesh.rotateY(1/5 * timeDelta);
-    // // Rotate Earth
-    // earthMesh.rotateY(1/5 * timeDelta);
-    earth.waterMesh.rotateY(1/5 * timeDelta);
-    // Rotate Earth
-    earth.mesh.rotateY(1/5 * timeDelta);
-    // Rotate clouds
-    earth.cloudMesh.rotateY(1/3 * timeDelta);
+// Create a scene with a camera and renderer
+  let scene = new THREE.Scene();
+  camera	= new THREE.PerspectiveCamera(
+    FIELD_OF_VIEW,
+    window.innerWidth / window.innerHeight,
+    NEAR,
+    FAR
+  );
+  camera.position.z = 1.5;
+
+
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // Add some light
+  // TODO: play with this so it's not exactly same as demo
+  let light	= new THREE.AmbientLight(0x888888);
+  scene.add(light);
+  let dirLight	= new THREE.DirectionalLight(0xcccccc, 0.5);
+  dirLight.position.set(5, 3, 5);
+  scene.add(dirLight);
+
+  // Create and add the Earth
+  earth = new Earth(EARTH_RAD, false, worldMap.canvas);
+  if (earth.waterMesh) {
+    scene.add(earth.waterMesh);
   }
-  if (isZooming) {
-    camera.position.z -= 0.007;
-    if (camera.position.z <= 1.1) {
-      isZooming = false;
-      isZoomedIn = true;
+  scene.add(earth.mesh);
+  earth.addClouds();
+
+  particleCloud = new ParticleCloud(
+    NUM_PARTICLES,
+    0.01,
+    0xffffff,
+    earth.cloudMesh,
+    earth.cloudRadius
+  );
+  earth.mesh.add(particleCloud.mesh);
+
+  // Render and update loop
+  let clock = new THREE.Clock();
+
+  // TODO in the end, after all animation, particles expand out infinitely and
+  // become stars!
+
+  function render(){
+    requestAnimationFrame(render);
+
+    let timeDelta = clock.getDelta(); // in seconds
+    if (isRotating) {
+      // Rotate Water
+      earth.waterMesh.rotateY(1/5 * timeDelta);
+      // Rotate Earth
+      earth.mesh.rotateY(1/5 * timeDelta);
+      // Rotate clouds
+      earth.cloudMesh.rotateY(1/3 * timeDelta);
     }
+    renderer.render(scene, camera);
+    TWEEN.update();
   }
-  if (isZoomingOut) {
-    camera.position.z += 0.007;
-    if (camera.position.z >= 1.505) {
-      isZoomingOut = false;
-      // We're zoomed back out, undo the rotation
-      // TODO this doesn't go back to where it should...
-      let rotation = spinToPoint(
-        zoomPoint.clone(),
-        // new THREE.Vector3(0, 0, 1)
-        earth.mesh.worldToLocal(new THREE.Vector3(0, 0, 1))
-      );
 
-      // TODO refactor so there's not so much repeated here ad in swarm code
-      let lineCurves = [];
-      for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
-        let startPoint = particleCloud.mesh.geometry.vertices[i];
-        // TODO could be replaced with simple updates in the render loop. Is that
-        // better or should I use Tween for everything for consistency??
-        // Create a 3d line segment between the two points.
-        lineCurves.push(
-          new THREE.LineCurve3(startPoint.clone(), particleCloud.initialPositions[i].clone())
-        );
-      }
-      // For each timestep, move the points to the spots along the line for
-      // that timestep, projected onto the clouds
-      let amountAbove = 0.02;
-      let animation = new TWEEN.Tween({t: 0}).to({t: 1}, 1000)
-        // .easing(TWEEN.Easing.Exponential.InOut)
-        .onUpdate(function (obj) {
-          for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
-            let newPos = particleCloud.vertInClouds(
-              lineCurves[i].getPoint(obj.t),
-              amountAbove + obj.t * (EXPANSION_MAX - amountAbove)
-            );
-            particleCloud.mesh.geometry.vertices[i] = newPos;
-          }
-          particleCloud.mesh.geometry.verticesNeedUpdate = true;
-        });
-
-      animation
-        .onComplete(function () {
-          shrinkingParticles = true;
-        }).start();
-      rotation
-        .onComplete(function () {
-          isRotating = true;
-        })
-        .start();
-    }
-  }
-  if (expandingParticles) {
-    if (expansionOffset >= EXPANSION_MAX) {
-      expandingParticles = false;
-      expansionOffset = 0;
-      swarmParticlesToPoint(zoomPoint.clone());
-    } else {
-      expansionOffset += 0.009;
-      for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
-        let particlePos = particleCloud.mesh.geometry.vertices[i];
-        particleCloud.mesh.geometry.vertices[i] = particleCloud.vertInClouds(particlePos, expansionOffset);
-      }
-      particleCloud.mesh.geometry.verticesNeedUpdate = true
-    }
-  }
-  if (shrinkingParticles) {
-    if (shrinkingOffset <= SHRINKING_MIN) {
-      shrinkingParticles = false;
-      shrinkingOffset = EXPANSION_MAX;
-    } else {
-      shrinkingOffset -= 0.009;
-      for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
-        let particlePos = particleCloud.mesh.geometry.vertices[i];
-        particleCloud.mesh.geometry.vertices[i] = particleCloud.vertInClouds(particlePos, shrinkingOffset);
-      }
-      particleCloud.mesh.geometry.verticesNeedUpdate = true
-    }
-  }
-  renderer.render(scene, camera);
-  TWEEN.update();
-}
-render();
+  render();
+});
 
 // Create global point from lat/long
 const edmondLat = 35.657295;
@@ -184,16 +95,44 @@ const edmondLong = -97.478256;
 let zoomPoint = spherePointFromLatLong(edmondLat, edmondLong);
 document.body.onclick = function () {
 
-  if (isZoomedIn == false) {
-    expandingParticles = true;
-    expansionOffset = 0;
+  if (!isZoomedIn) {
+    // Expand the particles
+    let expandTween = expandParticles(0, EXPANSION_MAX);
+    // Then rotate Earth and swarm to point simultaneously
+    // TODO make making this array neater, possible in sep function
+    let pointDestinations = []
+    for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
+      pointDestinations.push(particleCloud.vertInClouds(zoomPoint.clone(), AMOUNT_POINT_ABOVE));
+    }
+    let swarmTween = swarmParticlesToPoints(pointDestinations, EXPANSION_MAX, AMOUNT_POINT_ABOVE);
+
+    let spinTween = spinToPoint(new THREE.Vector3(0, 0 , 1), zoomPoint.clone());
+    // Then zoom in on the swarmed point
+    let zoomTween = zoomCamera(1.1, true)
+
+    expandTween.chain(swarmTween, spinTween.chain(zoomTween));
+    expandTween.start();
+
   } else {
-    isZoomingOut = true;
-    isZoomedIn = false;
+
+    // Zoom out
+    let zoomTween = zoomCamera(1.5, false);
+    // Swarm back to initial positions and rotate Earth simultaneously
+    let swarmTween = swarmParticlesToPoints(particleCloud.initialPositions, AMOUNT_POINT_ABOVE, EXPANSION_MAX);
+    let spinTween = spinToPoint(
+      zoomPoint.clone(),
+      // new THREE.Vector3(0, 0, 1)
+      earth.mesh.worldToLocal(new THREE.Vector3(0, 0, 1))
+    );
+    // Shrink the particles
+    let shrinkTween = expandParticles(EXPANSION_MAX, 0).onStart(function () {
+      isRotating = true;
+    });
+
+    zoomTween.chain(swarmTween, spinTween.chain(shrinkTween));
+    zoomTween.start();
   }
 
-  // camera.lookAt(zoomPoint);
-  // camera.lookAt(0, 0, 1);
 }
 
 // Returns Vector3 point on surface of earth mesh from
@@ -223,7 +162,7 @@ function spinToPoint(startPoint, endPoint) {
   );
 
   let euler = new THREE.Euler();
-  return new TWEEN.Tween(startQuant).to(endQuant, 1500)
+  let spinAnimation = new TWEEN.Tween(startQuant).to(endQuant, 1500)
     // .delay(500)
     // .easing(TWEEN.Easing.Exponential.InOut)
     .onUpdate(function () {
@@ -231,24 +170,20 @@ function spinToPoint(startPoint, endPoint) {
       earth.mesh.setRotationFromEuler(euler);
       earth.waterMesh.setRotationFromEuler(euler);
     });
+  return spinAnimation;
 }
 
-// Takes a Vector3 point on the sphere and animates particles swarming to
-// just above that point
-function swarmParticlesToPoint(point) {
-  // TODO play with this
-  isRotating = false;
-  let amountAbove = 0.02;
-  let endPoint = particleCloud.vertInClouds(point, amountAbove);
+// Takes an array of Vector3 point on the sphere and returns Tween animation
+// particle i swarming to point i. Particles will start at startOffset above
+// the clouds and end at endOffset above the clouds.
+function swarmParticlesToPoints(endPoints, startOffset, endOffset) {
   // Create array of line segments between particles' start positions
   // and the desired end position.
   let lineCurves = [];
   for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
     let startPoint = particleCloud.mesh.geometry.vertices[i];
-    // TODO could be replaced with simple updates in the render loop. Is that
-    // better or should I use Tween for everything for consistency??
     // Create a 3d line segment between the two points.
-    lineCurves.push(new THREE.LineCurve3(startPoint.clone(), endPoint.clone()));
+    lineCurves.push(new THREE.LineCurve3(startPoint.clone(), endPoints[i].clone()));
   }
   // For each timestep, move the points to the spots along the line for
   // that timestep, projected onto the clouds
@@ -258,26 +193,43 @@ function swarmParticlesToPoint(point) {
       for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
         let newPos = particleCloud.vertInClouds(
           lineCurves[i].getPoint(obj.t),
-          EXPANSION_MAX - (EXPANSION_MAX - amountAbove) * obj.t
+          startOffset + (endOffset - startOffset) * obj.t
         );
         particleCloud.mesh.geometry.vertices[i] = newPos;
       }
       particleCloud.mesh.geometry.verticesNeedUpdate = true;
+    })
+    .onStart(function () {
+      isRotating = false;
     });
 
-  let rotation = spinToPoint(
-    // earthMesh.worldToLocal(new THREE.Vector3(0, 0, 1)),
-    // earthPointFacingCamera(),
-    new THREE.Vector3(0, 0 , 1),
-    zoomPoint.clone()
-  );
-  rotation.onComplete(function () {
-    isZooming = true;
-  });
-
-  // Start at same time, don't need to be completely synchronized
-  rotation.start();
-  animation.start();
+  return animation;
 }
 
-});
+// Returns Tween that zooms camera from current position to endPosition.
+function zoomCamera(endPosition, zoomingIn) {
+  let zoomAnimation = new TWEEN.Tween({currentZoom: camera.position.z})
+    .to({currentZoom: endPosition}, 1000)
+    .onUpdate(function(obj) {
+      camera.position.z = obj.currentZoom;
+    })
+    .onComplete(function () {
+      isZoomedIn = zoomingIn;
+    });
+  return zoomAnimation;
+}
+
+// Returns Tween that expanda particles from startOffset above clouds to
+// endOffset above clouds
+function expandParticles(startOffset, endOffset) {
+  let expansionAnimation = new TWEEN.Tween({currentOffset: startOffset})
+    .to({currentOffset: endOffset}, 1000)
+    .onUpdate(function(obj) {
+      for (let i = 0; i < particleCloud.mesh.geometry.vertices.length; i++) {
+        let particlePos = particleCloud.mesh.geometry.vertices[i];
+        particleCloud.mesh.geometry.vertices[i] = particleCloud.vertInClouds(particlePos, obj.currentOffset);
+      }
+      particleCloud.mesh.geometry.verticesNeedUpdate = true
+    });
+  return expansionAnimation;
+}
